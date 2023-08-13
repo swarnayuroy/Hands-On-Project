3c8c26316fcef3a2dda3a631144d3d713096ac58
swarnayuroy <swarnayuroy@gmail.com>
2023-08-13 15:16:45 +0530

    created a sub-menu under greet head for profile editing

---
+++ AuthenticationJWT/AuthenticationJWT/Scripts/userhome-scripts.js
@@ -0,0 +1,10 @@
+﻿var subMenuState = false;
+function setSubMenuState() {
+    if (!subMenuState) {
+        document.getElementById("sub-menu").style.display = "block";
+        subMenuState = true;
+    } else {
+        document.getElementById("sub-menu").style.display = "none";
+        subMenuState = false;
+    }
+};
