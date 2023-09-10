817d031d537074dcc279beb78ed97a27f2c1351d
swarnayuroy <swarnayuroy@gmail.com>
2023-09-10 19:31:11 +0530

    have designed modal confirmation for deleting an user account

--- AuthenticationJWT/AuthenticationJWT/Scripts/userhome-scripts.js
+++ AuthenticationJWT/AuthenticationJWT/Scripts/userhome-scripts.js
@@ -1,4 +1,6 @@
 ﻿var subMenuState = false;
+var modalConfirmationState = false;
+
 function setSubMenuState() {
     if (!subMenuState) {
         document.getElementById("sub-menu").style.display = "block";
@@ -6,5 +8,15 @@ function setSubMenuState() {
     } else {
         document.getElementById("sub-menu").style.display = "none";
         subMenuState = false;
+    }
+};
+
+function setModalConfirmation() {
+    if (!modalConfirmationState) {
+        document.querySelector('.modal-confirmation').style.display = "flex";
+        modalConfirmationState = true;
+    } else {
+        document.querySelector('.modal-confirmation').style.display = "none";
+        modalConfirmationState = false;
     }
 };
